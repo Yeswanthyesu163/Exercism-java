5fd8b4819c8855acf5615db6c7de0f73f5d99884
Yeswanthyesu163 <yeswanth.cs18@bitsathy.ac.in>
2022-07-16 12:13:50 +0530

    Solved Rna-transcription Exercise

--- rna-transcription/src/main/java/RnaTranscription.java
+++ rna-transcription/src/main/java/RnaTranscription.java
@@ -1,7 +1,25 @@
+import java.util.*;
+
 class RnaTranscription {
 
     String transcribe(String dnaStrand) {
-        throw new UnsupportedOperationException("Delete this statement and write your own implementation.");
+        Hashtable<Character, Character> rna = new Hashtable<Character, Character>();
+
+        String answer = "";
+
+        rna.put('G','C');
+        rna.put('C','G');
+        rna.put('T','A');
+        rna.put('A','U');
+
+        for(int i =0; i < dnaStrand.length(); i++) {
+            char ch = rna.containsKey(dnaStrand.charAt(i))
+                    ? rna.get(dnaStrand.charAt(i))
+                    : dnaStrand.charAt(i);
+            answer += ch;
+        }
+
+        return answer;
     }
 
 }
