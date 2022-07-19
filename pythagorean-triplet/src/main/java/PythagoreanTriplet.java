105cf925a349a876d94ae362eb8a697f0bc8d53a
Yeswanthyesu163 <yeswanth.cs18@bitsathy.ac.in>
2022-07-19 16:30:44 +0530

    Completed Pythagorean Triplet Exercise

--- pythagorean-triplet/src/main/java/PythagoreanTriplet.java
+++ pythagorean-triplet/src/main/java/PythagoreanTriplet.java
@@ -1,10 +1,65 @@
-/*
+import java.util.*;
+public class PythagoreanTriplet {
 
-Since this exercise has a difficulty of > 4 it doesn't come
-with any starter implementation.
-This is so that you get to practice creating classes and methods
-which is an important part of programming in Java.
+    private int a;
+    private int b;
+    private int c;
 
-Please remove this comment when submitting your solution.
+    PythagoreanTriplet(int a, int b, int c) {
+        this.a = a;
+        this.b = b;
+        this.c =c;
+    }
 
-*/
+    @Override
+    public boolean equals(Object obj) {
+        if(obj == this) {
+            return true;
+        }
+        if(!(obj instanceof PythagoreanTriplet)) {
+            return false;
+        }
+        PythagoreanTriplet temp = (PythagoreanTriplet) obj;
+        return this.a == temp.a && this.b == temp.b && this.c == temp.c;
+    }
+
+
+
+    public static PythagoreanTripletBuilder makeTripletsList() {
+        return new PythagoreanTripletBuilder();
+    }
+
+    public static class PythagoreanTripletBuilder {
+        private int factor;
+        private int sum;
+
+        public PythagoreanTripletBuilder withFactorsLessThanOrEqualTo(int factor) {
+            this.factor = factor;
+            return this;
+        }
+
+        public PythagoreanTripletBuilder thatSumTo(int sum) {
+            this.sum = sum;
+            return this;
+        }
+
+        public List<PythagoreanTriplet> build() {
+            List<PythagoreanTriplet> triplets = new ArrayList<>();
+            int a,b,c;
+            for(a = 1; a < factor; a++) {
+                for(b = a + 1; b < factor; b++) {
+                    c = sum - a - b;
+                    if(c < a || c < b) {
+                        break;
+                    }
+                    if( (a*a) + (b*b) == (c*c)) {
+                        triplets.add(new PythagoreanTriplet(a,b,c));
+                    }
+                }
+            }
+            return  triplets;
+        }
+    }
+
+
+}
